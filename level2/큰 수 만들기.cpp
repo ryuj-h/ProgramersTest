3640bf336cd8fc549f2c9858ca4881a8fbe13fe3
Ryu-J <37369443+ryuj-h@users.noreply.github.com>
2022-03-21 19:53:14 +0900

    Create 큰 수 만들기.cpp

---
+++ level2/큰 수 만들기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string number, int k) {
+    string answer = "";
+    for (int j = 0, index = -1; j < number.length() - k; j++) {
+        char max = 0;
+        for (int i = index + 1; i <= k + j; i++) {
+            if (max < number[i]) {
+                index = i;
+                max = number[i];
+            }
+        }
+        answer += max;
+    }
+    return answer;
+}
